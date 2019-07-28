78044c1d07f3b1889f124a4ea774e2b363602f29
pavlov18 <topor001@mail.ru>
2019-07-28 16:00:41 +0400

    Завершение слайдера JS кроме верной анимации

--- source/js/js.js
+++ source/js/js.js
@@ -30,14 +30,19 @@ var btnNext = slider.querySelector(".reviews__button--next");
 var sliderContent = slider.querySelector(".slider__list");
 var firstSlide = sliderContent.firstElementChild;
 var finalSlide = sliderContent.lastElementChild;
+var sliderToggles = slider.querySelectorAll(".slider__toggle");
+var sliderTogglesList = slider.querySelector(".slider__toggles-list");
 
 function slideButtons() {
-  for (var i = 0; i < slideRadio.length; i++) {
+  for (i = 0; i < slideRadio.length; i++) {
+
     if (slideRadio[i].checked) {
+      numRadio =  i;
       currentCheck = slideRadio[i];
       nextRadio = slideRadio[i+1];
       prevRadio = slideRadio[i-1];
     }
+
     if (i === slideRadio.length - 1) {
       finalRadio = slideRadio[i];
     }
@@ -45,28 +50,96 @@ function slideButtons() {
       firstRadio = slideRadio[i];
     }
   }
+
 };
+
+
+function funcCurrentSlide() {
+  for (i = 0; i < slides.length; i++) {
+
+    if (slides[i].classList.contains("slider__item--current")) {
+      currentSlide = slides[i];
+      nextSlide = slides[i+1];
+      prevSlide = slides[i-1];
+    }
+
+    if (i === slides.length - 1) {
+      finalSlide = slides[i];
+    }
+    if (i === 0) {
+      firstSlide = slides[i];
+    }
+  }
+
+};
+
 
 btnNext.addEventListener("click", function (evt) {
   evt.preventDefault();
   slideButtons();
+  funcCurrentSlide();
   currentCheck.checked = false;
+  currentSlide.classList.remove("slider__item--current");
+  currentSlide.classList.remove("bounce-right","bounce-left");
+
   if (currentCheck != finalRadio) {
     nextRadio.checked = true;
+    nextSlide.classList.add("slider__item--current");
+    nextSlide.classList.add("bounce-left");
   }
+
   else {
     firstRadio.checked = true;
+    firstSlide.classList.add("slider__item--current");
+    firstSlide.classList.add("bounce-left");
   }
+
 });
 
 btnBack.addEventListener("click", function (evt) {
   evt.preventDefault();
   slideButtons();
+  funcCurrentSlide();
   currentCheck.checked = false;
+  currentSlide.classList.remove("slider__item--current");
+  currentSlide.classList.remove("bounce-right","bounce-left");
+
   if (currentCheck != firstRadio) {
     prevRadio.checked=true;
+    prevSlide.classList.add("slider__item--current");
+    prevSlide.classList.add("bounce-right");
   }
   else {
     finalRadio.checked = true;
+    finalSlide.classList.add("slider__item--current");
+    finalSlide.classList.add("bounce-right");
   }
 });
+
+
+
+
+for (var i=0 ; i < slideRadio.length; i++) {
+  if (slideRadio[i].checked) {
+    numRadioCheckedOld = i;
+  }
+  slideRadio[i].addEventListener("click", function() {
+    for (var i = 0; i < slideRadio.length; i++) {
+      if (slideRadio[i].checked) {
+        numRadioChecked = i + 1;
+        console.log('всего элементов - ' + slideRadio.length)
+        for (var i = 0; i < slides.length; i++) {
+          slides[i].classList.remove("slider__item--current");
+        }
+        numSlide = sliderContent.querySelector(".slider__item:nth-of-type(" + numRadioChecked + ")");
+        if (numRadioChecked > (slideRadio.length - numRadioChecked)) {
+          numSlide.classList.add("slider__item--current","bounce-left");
+        }
+        else {
+          numSlide.classList.add("slider__item--current","bounce-right");
+        }
+      }
+    }
+  });
+}
+
