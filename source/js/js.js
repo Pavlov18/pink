69978312dfb609c2e4684cee3b53c1019fcf3d80
Pavlov18 <topor001@mail.ru>
2019-07-29 11:39:20 +0400

    Завершение JS кнопок слайдера

--- source/js/js.js
+++ source/js/js.js
@@ -6,7 +6,6 @@ var mapIframe = document.querySelector(".contacts__map-iframe")
 mobileMenu.classList.remove("main-nav--nojs");
 mapIndex.classList.remove("contacts__map--nojs");
 
-
 navMobileButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (mobileMenu.classList.contains("main-nav--open")) {
@@ -32,27 +31,33 @@ var firstSlide = sliderContent.firstElementChild;
 var finalSlide = sliderContent.lastElementChild;
 var sliderToggles = slider.querySelectorAll(".slider__toggle");
 var sliderTogglesList = slider.querySelector(".slider__toggles-list");
+var oldChecked = slider.querySelector(".slider__button:checked");
 
-function slideButtons() {
+function ooold() {
   for (i = 0; i < slideRadio.length; i++) {
-
     if (slideRadio[i].checked) {
-      numRadio =  i;
-      currentCheck = slideRadio[i];
-      nextRadio = slideRadio[i+1];
-      prevRadio = slideRadio[i-1];
-    }
-
-    if (i === slideRadio.length - 1) {
-      finalRadio = slideRadio[i];
-    }
-    if (i === 0) {
-      firstRadio = slideRadio[i];
+        olded = i;
+      }
     }
   }
 
-};
+function slideButtons() {
+  for (i = 0; i < slideRadio.length; i++) {
+    if (slideRadio[i].checked) {
+        numChecked = i;
+        currentChecked  = slideRadio[i];
+        nextRadio = slideRadio[i+1];
+        prevRadio = slideRadio[i-1];
+    }
 
+    if (i === slideRadio.length - 1) {
+        finalRadio = slideRadio[i];
+    }
+    if (i === 0) {
+        firstRadio = slideRadio[i];
+    }
+  }
+};
 
 function funcCurrentSlide() {
   for (i = 0; i < slides.length; i++) {
@@ -70,19 +75,18 @@ function funcCurrentSlide() {
       firstSlide = slides[i];
     }
   }
-
 };
-
 
 btnNext.addEventListener("click", function (evt) {
   evt.preventDefault();
   slideButtons();
   funcCurrentSlide();
-  currentCheck.checked = false;
+
+  currentChecked.checked = false;
   currentSlide.classList.remove("slider__item--current");
   currentSlide.classList.remove("bounce-right","bounce-left");
 
-  if (currentCheck != finalRadio) {
+  if (currentChecked != finalRadio) {
     nextRadio.checked = true;
     nextSlide.classList.add("slider__item--current");
     nextSlide.classList.add("bounce-left");
@@ -91,55 +95,55 @@ btnNext.addEventListener("click", function (evt) {
   else {
     firstRadio.checked = true;
     firstSlide.classList.add("slider__item--current");
-    firstSlide.classList.add("bounce-left");
+    firstSlide.classList.add("bounce-right");
   }
-
 });
 
 btnBack.addEventListener("click", function (evt) {
   evt.preventDefault();
   slideButtons();
   funcCurrentSlide();
-  currentCheck.checked = false;
+  currentChecked.checked = false;
   currentSlide.classList.remove("slider__item--current");
   currentSlide.classList.remove("bounce-right","bounce-left");
 
-  if (currentCheck != firstRadio) {
+  if (currentChecked != firstRadio) {
     prevRadio.checked=true;
     prevSlide.classList.add("slider__item--current");
     prevSlide.classList.add("bounce-right");
   }
+
   else {
     finalRadio.checked = true;
     finalSlide.classList.add("slider__item--current");
-    finalSlide.classList.add("bounce-right");
+    finalSlide.classList.add("bounce-left");
   }
 });
 
-
-
-
-for (var i=0 ; i < slideRadio.length; i++) {
-  if (slideRadio[i].checked) {
-    numRadioCheckedOld = i;
-  }
-  slideRadio[i].addEventListener("click", function() {
-    for (var i = 0; i < slideRadio.length; i++) {
-      if (slideRadio[i].checked) {
-        numRadioChecked = i + 1;
-        console.log('всего элементов - ' + slideRadio.length)
-        for (var i = 0; i < slides.length; i++) {
-          slides[i].classList.remove("slider__item--current");
-        }
-        numSlide = sliderContent.querySelector(".slider__item:nth-of-type(" + numRadioChecked + ")");
-        if (numRadioChecked > (slideRadio.length - numRadioChecked)) {
-          numSlide.classList.add("slider__item--current","bounce-left");
-        }
-        else {
-          numSlide.classList.add("slider__item--current","bounce-right");
+sliderTogglesList.addEventListener("click", function() {
+  slideButtons();
+  for (var i = 0; i < slideRadio.length; i++) {
+    slideRadio[i].addEventListener("click", function() {
+      for (var i = 0; i < slideRadio.length; i++) {
+        if (slideRadio[i].checked) {
+          numCheck = i + 1;
+          for (var i = 0; i < slides.length; i++) {
+            slides[i].classList.remove("slider__item--current","bounce-left","bounce-right");
+          }
+          numSlide = sliderContent.querySelector(".slider__item:nth-of-type(" + numCheck + ")");
         }
       }
-    }
-  });
-}
+      if (numCheck > (numChecked + 1)) {
+        numSlide.classList.add("slider__item--current","bounce-left");
+      }
 
+      if (numCheck < (numChecked + 1)) {
+        numSlide.classList.add("slider__item--current","bounce-right");
+      }
+
+      if (numCheck === (numChecked + 1)) {
+        numSlide.classList.add("slider__item--current");
+      }
+    });
+  }
+});
